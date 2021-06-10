db5e9bcc4e4414cba386495b30e3b0a7ce8d6fa9
zhoulong.yang <zhoulong.yang@xrkmm.com>
2021-06-10 16:39:40 +0800

    .

--- example-react/src/App.tsx
+++ example-react/src/App.tsx
@@ -3,11 +3,11 @@
  * @Author: zhoulong.yang
  * @Date: 2021-06-09 15:04:03
  * @LastEditors: zhoulong.yang
- * @LastEditTime: 2021-06-10 15:11:32
+ * @LastEditTime: 2021-06-10 16:35:14
  */
 import { Card } from '@xrkmm/ui'
 import './App.css'
-import '@xrkmm/ui/styles/index.scss'
+import '../../dist/xrkmm.mini.css'
 
 function App() {
   return (
