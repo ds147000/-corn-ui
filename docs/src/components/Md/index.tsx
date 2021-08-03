92ab56b8bb1526ed302b1c03569392f2466271a9
zhoulong.yang <zhoulong.yang@xrkmm.com>
2021-08-03 16:11:56 +0800

    增加编译ui库在线文档命令

--- docs/src/components/Md/index.tsx
+++ docs/src/components/Md/index.tsx
@@ -59,7 +59,7 @@ demo
 这是一个普通的卡片，具有标题和点击事件
 </code></pre>
 <h3>生成文档预览</h3>
-<img width="700" src="https://assets.xrkmm.cn/u/4000002499670412/bbbe7e74-8a6d-4348-a742-cf26559999b2.png">
+<img style="width:100%" src="https://assets.xrkmm.cn/u/4000002499670412/bbbe7e74-8a6d-4348-a742-cf26559999b2.png">
 `,
       }}
     />
--- docs/src/components/history/index.tsx
+++ docs/src/components/history/index.tsx
@@ -1,19 +1,5 @@
 const Page: React.FC = () => {
-  return (
-    <div
-      className="scope-page"
-      dangerouslySetInnerHTML={{
-        __html: `<!--
- * @Description:
- * @Author: zhoulong.yang
- * @Date: 2021-06-10 19:52:34
- * @LastEditors: zhoulong.yang
- * @LastEditTime: 2021-06-10 19:52:38
--->
-`,
-      }}
-    />
-  )
+  return <div className="scope-page" dangerouslySetInnerHTML={{ __html: `` }} />
 }
 
 export default Page
