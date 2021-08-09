d416741c08d8a83f8108300263a91a97160a4f9e
zhoulong.yang <zhoulong.yang@xrkmm.com>
2021-08-09 12:22:56 +0800

    .

--- docs/src/components/history/index.tsx
+++ docs/src/components/history/index.tsx
@@ -1,5 +1,20 @@
 const Page: React.FC = () => {
-  return <div className="scope-page" dangerouslySetInnerHTML={{ __html: `` }} />
+  return (
+    <div
+      className="scope-page"
+      dangerouslySetInnerHTML={{
+        __html: `<h1>0.0.5-alpha2</h1>
+<ul>
+<li>添加 <code><span class="hljs-selector-tag">Button</span></code></li>
+<li>添加 <code><span class="hljs-attribute">Actionsheet</span></code></li>
+<li>添加 <code><span class="hljs-attribute">Drawer</span></code></li>
+<li>添加 <code><span class="hljs-attribute">Toast</span></code></li>
+<li>优化构建，支持webpack自动化esm构建引入。优化依赖树，实现按需编译组件</li>
+</ul>
+`,
+      }}
+    />
+  )
 }
 
 export default Page
--- src/index.ts
+++ src/index.ts
@@ -5,11 +5,11 @@
  * @LastEditors: zhoulong.yang
  * @LastEditTime: 2021-06-07 16:08:34
  */
-import { Swiper } from '@tarojs/components'
+import { showLoading } from '@tarojs/taro'
 
 export { default as Button, ButtonProps } from './components/Button'
 export { default as Toast } from './components/Toast'
 export { default as Drawer } from './components/Drawer'
 export { default as ActionSheet, ActionSheetItem } from './components/ActionSheet'
 export { default as showActionSheet } from './components/ActionSheet/api'
-export { Swiper }
+export { showLoading }
