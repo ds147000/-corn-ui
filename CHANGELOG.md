54a7ad71dc4f857b82add12b0fa57e7c91d19cf6
zhoulong.yang <zhoulong.yang@xrkmm.com>
2021-08-23 16:28:47 +0800

    发布0.0.13

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,20 +1,8 @@
-# 0.0.5-alpha2
- - 添加 `Button`
- - 添加 `Actionsheet`
- - 添加 `Drawer`
- - 添加 `Toast`
- - 优化构建，支持webpack自动化esm构建引入。优化依赖树，实现按需编译组件
+ # 0.0.13
+ - 添加 `Tgb`
 
-
-# 0.0.6
- - 添加 `Modal`
-
-
-# 0.0.8
- - 添加 `Icon`
-
- # 0.0.9
- - 添加 `Empty`
+ # 0.0.12
+ - 添加 `Tab`
 
  # 0.0.11
  - 添加 `Money`
@@ -22,5 +10,20 @@
  - 添加 `previewImage`
 
 
- # 0.0.12
- - 添加 `Tab`
+ # 0.0.9
+ - 添加 `Empty`
+
+# 0.0.8
+ - 添加 `Icon`
+
+# 0.0.6
+ - 添加 `Modal`
+
+
+# 0.0.5-alpha2
+ - 添加 `Button`
+ - 添加 `Actionsheet`
+ - 添加 `Drawer`
+ - 添加 `Toast`
+ - 优化构建，支持webpack自动化esm构建引入。优化依赖树，实现按需编译组件
+
--- docs/src/components/history/index.tsx
+++ docs/src/components/history/index.tsx
@@ -3,25 +3,13 @@ const Page: React.FC = () => {
     <div
       className="scope-page"
       dangerouslySetInnerHTML={{
-        __html: `<h1>0.0.5-alpha2</h1>
+        __html: `<h1>0.0.13</h1>
 <ul>
-<li>添加 <code><span class="hljs-selector-tag">Button</span></code></li>
-<li>添加 <code><span class="hljs-attribute">Actionsheet</span></code></li>
-<li>添加 <code><span class="hljs-attribute">Drawer</span></code></li>
-<li>添加 <code><span class="hljs-attribute">Toast</span></code></li>
-<li>优化构建，支持webpack自动化esm构建引入。优化依赖树，实现按需编译组件</li>
+<li>添加 <code><span class="hljs-attribute">Tgb</span></code></li>
 </ul>
-<h1>0.0.6</h1>
+<h1>0.0.12</h1>
 <ul>
-<li>添加 <code><span class="hljs-attribute">Modal</span></code></li>
-</ul>
-<h1>0.0.8</h1>
-<ul>
-<li>添加 <code><span class="hljs-attribute">Icon</span></code></li>
-</ul>
-<h1>0.0.9</h1>
-<ul>
-<li>添加 <code><span class="hljs-attribute">Empty</span></code></li>
+<li>添加 <code><span class="hljs-literal">Tab</span></code></li>
 </ul>
 <h1>0.0.11</h1>
 <ul>
@@ -29,9 +17,25 @@ const Page: React.FC = () => {
 <li>添加 <code><span class="hljs-attribute">Image</span></code></li>
 <li>添加 <code><span class="hljs-attribute">previewImage</span></code></li>
 </ul>
-<h1>0.0.12</h1>
+<h1>0.0.9</h1>
 <ul>
-<li>添加 <code><span class="hljs-literal">Tab</span></code></li>
+<li>添加 <code><span class="hljs-attribute">Empty</span></code></li>
+</ul>
+<h1>0.0.8</h1>
+<ul>
+<li>添加 <code><span class="hljs-attribute">Icon</span></code></li>
+</ul>
+<h1>0.0.6</h1>
+<ul>
+<li>添加 <code><span class="hljs-attribute">Modal</span></code></li>
+</ul>
+<h1>0.0.5-alpha2</h1>
+<ul>
+<li>添加 <code><span class="hljs-selector-tag">Button</span></code></li>
+<li>添加 <code><span class="hljs-attribute">Actionsheet</span></code></li>
+<li>添加 <code><span class="hljs-attribute">Drawer</span></code></li>
+<li>添加 <code><span class="hljs-attribute">Toast</span></code></li>
+<li>优化构建，支持webpack自动化esm构建引入。优化依赖树，实现按需编译组件</li>
 </ul>
 `,
       }}
