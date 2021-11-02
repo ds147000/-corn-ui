05287829b2363aecdb9d89f4d643c6cebe34830d
zhoulong.yang <zhoulong.yang@xrkmm.com>
2021-11-02 11:47:57 +0800

    .

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -2,11 +2,17 @@ import { View } from '@tarojs/components'
 import React from 'react'
 
 export interface SearchProps {
+  /** 风格类型*/
   type?: 'default' | 'light'
+  /** 启用返回按钮 */
   back?: boolean
+  /** 尾部内容 */
   suffix?: React.ReactNode
+  /** 可输入 */
+  openInput?: boolean
+  /** 点击输入框事件 */
   onClick?(): void
-  input?: boolean
+  /** 输入发生改变事件 */
   onInput?(value: string): void
 }
 
